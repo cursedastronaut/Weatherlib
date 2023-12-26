c3254f48c1ffa69421aa44d94a72884d6923502f
cursedastronaut <g.martineaux@student.isartdigital.com>
2023-12-26 16:25:49 +0100

    Fixed CPPWeather returning nothing.

--- weather.cpp
+++ weather.cpp
@@ -36,40 +36,42 @@ string CPPWeather::GetUserIP() {
 }
 
 
-void CPPWeather::GetWeather() {
+weather CPPWeather::GetWeather() {
 	RefreshUserIP();
 	CURL *curl;
 	CURLcode res;
-	std::string request = "https://api.CPPWeather.com/v1/current.json?key=";
+	std::string request = "https://api.weatherapi.com/v1/current.json?key=";
 	request += token;
 	request +="&q=";
 	request += ip;
 	request += "&aqi=no/current.json";
-	
+	const char * tempRequest = request.c_str();
 	curl = curl_easy_init();
-	if(curl) {
-		curl_easy_setopt(curl, CURLOPT_URL, request.c_str());
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &apiAnswer);
-		res = curl_easy_perform(curl);
-		curl_easy_cleanup(curl);
+	if(!curl)
+		return weather();
+	curl_easy_setopt(curl, CURLOPT_URL, tempRequest);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &apiAnswer);
+	res = curl_easy_perform(curl);
+	curl_easy_cleanup(curl);
 
-		json temp = json::parse(apiAnswer);
-		temperatureCelsius =		temp["current"]["temp_c"];
-		temperatureFahrenheit =		temp["current"]["temp_f"];
-		windMph =					temp["current"]["wind_mph"];
-		windKph =					temp["current"]["wind_kph"];
-		windDegree =				temp["current"]["wind_degree"];
-		windDirection =				temp["current"]["wind_dir"];
-		pressureMillibars =			temp["current"]["pressure_mb"];
-		pressureInches =			temp["current"]["pressure_in"];
-		precipitationMillimeters =	temp["current"]["precip_mm"];
-		precipitationInches =		temp["current"]["precip_in"];
-		humidity =					temp["current"]["humidity"];
-		cloudCoverage =				temp["current"]["cloud"];
-		int isDayTemp =				temp["current"]["is_day"];
-		uv =						temp["current"]["uv"];
-		isDay = isDayTemp == 1;
-	}
+	json temp = json::parse(apiAnswer);
+	weather currentWeather;
+	currentWeather.temperatureCelsius =			temp["current"]["temp_c"];
+	currentWeather.temperatureFahrenheit =		temp["current"]["temp_f"];
+	currentWeather.windMph =					temp["current"]["wind_mph"];
+	currentWeather.windKph =					temp["current"]["wind_kph"];
+	currentWeather.windDegree =					temp["current"]["wind_degree"];
+	currentWeather.windDirection =				temp["current"]["wind_dir"];
+	currentWeather.pressureMillibars =			temp["current"]["pressure_mb"];
+	currentWeather.pressureInches =				temp["current"]["pressure_in"];
+	currentWeather.precipitationMillimeters =	temp["current"]["precip_mm"];
+	currentWeather.precipitationInches =		temp["current"]["precip_in"];
+	currentWeather.humidity =					temp["current"]["humidity"];
+	currentWeather.cloudCoverage =				temp["current"]["cloud"];
+	int isDayTemp =								temp["current"]["is_day"];
+	currentWeather.uv =							temp["current"]["uv"];
+	currentWeather.isDay = isDayTemp == 1;
+	return currentWeather;
 
 }
--- weather.hpp
+++ weather.hpp
@@ -13,33 +13,37 @@
 #include <curl/curl.h>
 #include <nlohmann/json.hpp>
 
+struct weather {
+	using string = std::string;
+	double	temperatureCelsius;
+	float	temperatureFahrenheit;
+	float	windMph;
+	float	windKph;
+	int		windDegree;
+	string	windDirection;
+	float	pressureMillibars;
+	float	pressureInches;
+	float	precipitationMillimeters;
+	float	precipitationInches;
+	int		humidity;
+	int		cloudCoverage;
+	bool	isDay;
+	int		uv;
+};
+
 class CPPWeather {
 	using string = std::string;
 	public:
 		CPPWeather(const string &);
 		void RefreshUserIP();
 		void Refresh();
-		void GetWeather();
+		weather GetWeather();
 		string GetUserIP();
 
 	private:
 		string token;
 		std::string ip = "";
 		string apiAnswer;
-		double	temperatureCelsius;
-		float	temperatureFahrenheit;
-		float	windMph;
-		float	windKph;
-		int		windDegree;
-		string	windDirection;
-		float	pressureMillibars;
-		float	pressureInches;
-		float	precipitationMillimeters;
-		float	precipitationInches;
-		int		humidity;
-		int		cloudCoverage;
-		bool	isDay;
-		int		uv;
 
 
 		static size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream);
